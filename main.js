264e5c03bb5844239aa474384cc3e2e6fb9562d2
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-06-02 11:24:16 -0700

    completed code along

---
+++ main.js
@@ -0,0 +1,106 @@
+console.log('connected')
+
+const getAllBtn = document.querySelector('#all')
+const charBtns = document.querySelectorAll('.char-btns')
+const ageForm = document.querySelector('#age-form')
+const ageInput = document.querySelector('#age-input')
+const createForm = document.querySelector('#create-form')
+const newFirstInput = document.querySelector('#first')
+const newLastInput = document.querySelector('#last')
+const newGenderDropDown = document.querySelector('select')
+const newAgeInput = document.querySelector('#age')
+const newLikesText = document.querySelector('textarea')
+const charContainer = document.querySelector('section')
+
+//this is found by runnign node server.js take the answer and put it in a string with https
+const baseURL = `http://localhost:4000`
+
+//below creates a new div and then createds the inner 
+//html with a strig that has a structure for how we want to info from the server,js 
+//Objcect to be broken down 
+
+function createCharacterCard(char) {
+  let charCard = document.createElement('div')
+  charCard.innerHTML = `<h3>${char.firstName} ${char.lastName}</h3>
+  <p>gender: ${char.gender} | age: ${char.age}</p>
+  <h4>Likes</h4>
+  <ul>
+    <li>${char.likes[0]}</li>
+    <li>${char.likes[1]}</li>
+    <li>${char.likes[2]}</li>
+  </ul>`
+
+  charContainer.appendChild(charCard)
+}
+
+function clearCharacters() {
+  charContainer.innerHTML = ``
+}
+
+const getAllChars = (event) => {
+  clearCharacters()
+  axios.get(baseURL + `/characters`)
+  .then((response) => {
+    console.log(response)
+    response.data.forEach(character=> {
+      createCharacterCard(character)
+    });
+  })
+  .catch((error)=>{
+    console.log(error)
+  })
+}
+
+const getChar = evt => {
+  console.log(evt.target)
+  clearCharacters()
+  axios.get(baseURL + `/character/${evt.target.id}`)
+  .then( response => {
+    console.log(response.data)
+    createCharacterCard(response.data)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+}
+
+const getCharByAge = evt =>{
+  evt.preventDefault()
+  clearCharacters()
+  axios.get(baseURL + `/character?age=${ageInput.value}`)
+  .then(response => {
+    response.data.forEach(charObj => createCharacterCard(charObj))
+  })
+  .catch(error =>{
+    console.log(error)
+  })
+}
+
+const createChar = event => {
+  event.preventDefault()
+  clearCharacters()
+  //this is the new body object we will be sending with our request
+  let body = {
+    firstName: newFirstInput.value,
+    lastName: newLastInput.value,
+    gender: newGenderDropDown.value,
+    age: newAgeInput.value,
+    likes: newLikesText.value.split(`,`)
+  }
+  axios.post(baseURL + `/character`, body)
+  .then(response => {
+    response.data.forEach(charObj => createCharacterCard(charObj))
+  })
+  .catch(error =>{
+    console.log(error)
+  })
+}
+
+getAllBtn.addEventListener(`click`, getAllChars)
+for (let i = 0; i < charBtns.length; i++){
+  charBtns[i].addEventListener(`click`, getChar)
+}
+
+ageForm.addEventListener(`submit`, getCharByAge)
+
+createForm.addEventListener(`submit`, createChar)
